12878e8e83fe60d56565677c494eb8ff347003f2
Mateusz kulinowski <vuzix3333@gmail.com>
2022-01-31 23:54:26 +0100

    update calendar when adding event

--- FunPlanner/Models/UserLoginDto.cs
+++ FunPlanner/Models/UserLoginDto.cs
@@ -4,9 +4,7 @@ namespace FunPlanner.Models
 {
     public class UserLoginDto
     {
-        [Required(ErrorMessage = "Proszę wpisać e-mail")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Proszę wpisać hasło")]
         public string Password { get; set; }
     }
 }
